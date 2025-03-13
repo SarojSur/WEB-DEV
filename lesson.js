98ca8e87726f64281950fdbdfcaf1e3e86081e90
Sarojsur <rathindra101@gmail.com>
2025-03-13 22:45:24 +0600

    Add files via upload

---
+++ lesson.js
@@ -0,0 +1,65 @@
+//Operator
+
+//Arithmatic Operator
+
+// let a = 5
+
+// let b = 6
+
+// let c = a+b
+
+// let d = a-b
+
+// let e = a*b
+
+// let f = a/b
+
+// // Logical Operator
+
+// let p = 5
+
+// let q = 5
+
+// let r = 6
+
+// let s = 7
+
+// Logic Gate
+
+// AND OR
+
+//Truth Table
+
+// AND
+
+// T T - T
+
+// F F - F
+
+// T F - F
+
+// F T - F
+
+// OR
+
+// T T - T
+
+// F F - F
+
+// T F - T
+
+// F T - T
+
+// let a = 5
+
+// let b = 6
+
+// let c = 7
+
+// let d =8
+
+//Conditional Staterment
+
+// let username = "Tabin";
+
+// let password = 1234;
